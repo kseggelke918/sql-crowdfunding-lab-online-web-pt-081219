5e6e7e0ab2b7be8de244f3d17163fd897f21a93d
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-17 16:46:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,24 @@
 INSERT INTO users (name, age) VALUES
-  ("Bob", 30)
+  ("Bob", 30), 
+  ("Steve", 25),
+  ("Katie", 34),
+  ("Mary", 22),
+  ("Angie", 29),
+  ("Michele", 52),
+  ("Justin", 41),
+  ("Buck", 38),
+  ("Sally", 31),
+  ("John", 62),
+  ("Karen", 62),
+  ("Keith", 47), 
+  ("Kevin", 48),
+  ("Kris", 45),
+  ("Kyle", 43),
+  ("Terry", 43),
+  ("Lori", 45),
+  ("Meg", 46),
+  ("Melissa", 34), 
+  ("JJ", 35)
+  
+  
   
