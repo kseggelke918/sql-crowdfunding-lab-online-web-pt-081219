a71c3056cba180d087efc06dc67e96a27800b47b
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-17 16:47:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -20,7 +20,8 @@ INSERT INTO users (name, age) VALUES
   ("Melissa", 34), 
   ("JJ", 35)
   
-INSERT INTO projects ()
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
+("title1", "category1", 100000, "1/1/2020", "6/1/2020"),
   
   
   
