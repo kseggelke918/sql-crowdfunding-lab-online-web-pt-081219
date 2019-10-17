c4bd9efbf167ef8f98f9e0953c829a76e9aaee1c
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-17 18:32:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, SUM(pledges.amount) FROM pledges
   INNER JOIN projects ON projects.id = pledges.project_id
-  GROUP BY project.title
+  GROUP BY projecs.title
   ORDER BY projects.title;"
 end
 
